a5d0a8ce3c3ad30a71d3a472053cbf85866712e8
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:29 +0300

    improve

--- admin/vendor/countdowntime/countdowntime.js
+++ admin/vendor/countdowntime/countdowntime.js
@@ -1,45 +1,34 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    function getTimeRemaining(endtime) { 
-      var t = Date.parse(endtime) - Date.parse(new Date());
-      var seconds = Math.floor((t / 1000) % 60);
-      var minutes = Math.floor((t / 1000 / 60) % 60);
-      var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-      var days = Math.floor(t / (1000 * 60 * 60 * 24));
-      return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-      };
-    }
+  // Function to get the time remaining until the deadline
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((t / 1000) % 60);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
 
-    function initializeClock(id, endtime) { 
-      var daysSpan = $('.days');
-      var hoursSpan = $('.hours');
-      var minutesSpan = $('.minutes');
-      var secondsSpan = $('.seconds');
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
 
-      function updateClock() { 
-        var t = getTimeRemaining(endtime);
+  // Function to initialize the clock
+  function initializeClock(id, endtime) {
+    const daysSpan = $(".days");
+    const hoursSpan = $(".hours");
+    const minutesSpan = $(".minutes");
+    const secondsSpan = $(".seconds");
 
-        daysSpan.html(t.days);
-        hoursSpan.html(('0' + t.hours).slice(-2));
-        minutesSpan.html(('0' + t.minutes).slice(-2));
-        secondsSpan.html(('0' + t.seconds).slice(-2))
+    // Method to update the clock
+    function updateClock() {
+      const time_remaining = getTimeRemaining(endtime);
 
-        if (t.total <= 0) {
-          clearInterval(timeinterval);
-        }
-      }
-
-      updateClock();
-      var timeinterval = setInterval(updateClock, 1000);
-    }
-
-    var deadline = new Date(Date.parse(new Date()) + 25 * 24 * 60 * 60 * 1000 + 13 * 60 * 60 * 1000); 
-    initializeClock('clockdiv', deadline);
-
-})(jQuery);
+      daysSpan.html(`${time_remaining.days}`);
+      hoursSpan.html(`${("0" + time_remaining.hours).slice(-2)}`);
+      minutesSpan.html(`${("0
