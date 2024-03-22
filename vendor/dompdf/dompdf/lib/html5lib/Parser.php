e0e29098fa1cc5e0162a9a3cf1ab97b1a6d32522
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:00 +0300

    improve

--- vendor/dompdf/dompdf/lib/html5lib/Parser.php
+++ vendor/dompdf/dompdf/lib/html5lib/Parser.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require_once dirname(__FILE__) . '/Data.php';
 require_once dirname(__FILE__) . '/InputStream.php';
 require_once dirname(__FILE__) . '/TreeBuilder.php';
@@ -12,26 +14,36 @@ class HTML5_Parser
 {
     /**
      * Parses a full HTML document.
-     * @param $text | HTML text to parse
-     * @param $builder | Custom builder implementation
-     * @return DOMDocument|DOMNodeList Parsed HTML as DOMDocument
+     *
+     * @param string                                 $text      HTML text to parse
+     * @param null|HTML5_Tokenizer&HTML5_BuilderInterface $builder Custom builder implementation
+     *
+     * @return DOMDocument Parsed HTML as DOMDocument
      */
-    public static function parse($text, $builder = null) {
+    public static function parse(string $text, ?HTML5_Tokenizer &$builder = null): DOMDocument
+    {
         $tokenizer = new HTML5_Tokenizer($text, $builder);
         $tokenizer->parse();
-        return $tokenizer->save();
+
+        $dom = $tokenizer->save();
+
+        if ($dom === false) {
+            throw new RuntimeException('Failed to save parsed HTML as DOMDocument.');
+        }
+
+        return $dom;
     }
 
     /**
      * Parses an HTML fragment.
-     * @param $text | HTML text to parse
-     * @param $context String name of context element to pretend parsing is in.
-     * @param $builder | Custom builder implementation
-     * @return DOMDocument|DOMNodeList Parsed HTML as DOMDocument
+     *
+     * @param string                                 $text      HTML text to parse
+     * @param string|null                              $context String name of context element to pretend parsing is in.
+     * @param null|HTML5_Tokenizer&HTML5_BuilderInterface $builder Custom builder implementation
+     *
+     * @return DOMNode Parsed HTML as DOMNode
      */
-    public static function parseFragment($text, $context = null, $builder = null) {
+    public static function parseFragment(string $text, ?string $context = null, ?HTML5_Tokenizer &$builder = null): DOMNode
+    {
         $tokenizer = new HTML5_Tokenizer($text, $builder);
-        $tokenizer->parseFragment($context);
-        return $tokenizer->save();
-    }
-}
+
