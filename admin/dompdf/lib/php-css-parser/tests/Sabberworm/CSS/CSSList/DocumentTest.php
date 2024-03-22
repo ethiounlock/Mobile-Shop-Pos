ad6b5ce0c558ecf4b53d7107757749118fc3c8f7
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:11 +0300

    improve

--- admin/dompdf/lib/php-css-parser/tests/Sabberworm/CSS/CSSList/DocumentTest.php
+++ admin/dompdf/lib/php-css-parser/tests/Sabberworm/CSS/CSSList/DocumentTest.php
@@ -3,24 +3,37 @@
 namespace Sabberworm\CSS\CSSList;
 
 use Sabberworm\CSS\Parser;
+use Sabberworm\CSS\Document;
 
+/**
+ * Test for Document class
+ */
 class DocumentTest extends \PHPUnit_Framework_TestCase {
 
-	public function testOverrideContents() {
-		$sCss = '.thing { left: 10px; }';
-		$oParser = new Parser($sCss);
-		$oDoc = $oParser->parse();
-		$aContents = $oDoc->getContents();
-		$this->assertCount(1, $aContents);
+    /**
+     * Test overriding contents of a CSS document
+     */
+    public function testOverrideContents() {
+        $sCss = '.thing { left: 10px; }';
+        $oParser = new Parser($sCss);
+        $oDoc = $oParser->parse();
 
-		$sCss2 = '.otherthing { right: 10px; }';
-		$oParser2 = new Parser($sCss);
-		$oDoc2 = $oParser2->parse();
-		$aContents2 = $oDoc2->getContents();
+        // Get the initial contents of the document
+        $initialContents = $oDoc->getContents();
+        $this->assertCount(1, $initialContents);
 
-		$oDoc->setContents(array($aContents[0], $aContents2[0]));
-		$aFinalContents = $oDoc->getContents();
-		$this->assertCount(2, $aFinalContents);
-	}
+        $sCss2 = '.otherthing { right: 10px; }';
+        $oParser2 = new Parser($sCss2);
+        $oDoc2 = $oParser2->parse();
+
+        // Override the contents of the document
+        $oDoc->setContents(array_merge($initialContents, $oDoc2->getContents()));
+        $aFinalContents = $oDoc->getContents();
+
+        // Check if the contents have been overridden correctly
+        $this->assertCount(2, $aFinalContents);
+        $this->assertEquals('.thing { left: 10px; }', $aFinalContents[0]);
+        $this->assertEquals('.otherthing { right: 10px; }', $aFinalContents[1]);
+    }
 
 }
