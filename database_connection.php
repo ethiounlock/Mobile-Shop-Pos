ba028845ba9bd611e2ba7dd3650d448e56b6a263
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:50 +0300

    improve

--- database_connection.php
+++ database_connection.php
@@ -1,6 +1,11 @@
 <?php
-//database_connection.php
-$connect = new PDO('mysql:host=localhost;dbname=project1', 'root', '123456789');
-$conn= mysqli_connect('localhost','root','123456789');
-$db=mysqli_select_db($conn,'project1');
+// database_connection.php
+
+try {
+    $connect = new PDO('mysql:host=localhost;dbname=project1;charset=utf8', 'root', '123456789');
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
 ?>
