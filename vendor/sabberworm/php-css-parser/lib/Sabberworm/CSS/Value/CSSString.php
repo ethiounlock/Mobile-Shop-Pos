2caa66ed875d37f05d9a5141d6c287dc607294f6
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:38 +0300

    improve

--- vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Value/CSSString.php
+++ vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Value/CSSString.php
@@ -4,63 +4,88 @@ namespace Sabberworm\CSS\Value;
 
 use Sabberworm\CSS\Parsing\ParserState;
 use Sabberworm\CSS\Parsing\SourceException;
+use Sabberworm\CSS\OutputFormat;
 
-class CSSString extends PrimitiveValue {
+class CSSString extends PrimitiveValue
+{
+    /** @var string */
+    private $string;
 
-	private $sString;
+    /**
+     * CSSString constructor.
+     *
+     * @param string $string
+     * @param int    $lineNo
+     */
+    public function __construct(string $string, int $lineNo = 0)
+    {
+        $this->string = $string;
+        parent::__construct($lineNo);
+    }
 
-	public function __construct($sString, $iLineNo = 0) {
-		$this->sString = $sString;
-		parent::__construct($iLineNo);
-	}
+    /**
+     * @param ParserState $oParserState
+     *
+     * @return static
+     * @throws SourceException
+     */
+    public static function parse(ParserState $oParserState): self
+    {
+        $sBegin = $oParserState->peek();
+        $sQuote = null;
 
-	public static function parse(ParserState $oParserState) {
-		$sBegin = $oParserState->peek();
-		$sQuote = null;
-		if ($sBegin === "'") {
-			$sQuote = "'";
-		} else if ($sBegin === '"') {
-			$sQuote = '"';
-		}
-		if ($sQuote !== null) {
-			$oParserState->consume($sQuote);
-		}
-		$sResult = "";
-		$sContent = null;
-		if ($sQuote === null) {
-			// Unquoted strings end in whitespace or with braces, brackets, parentheses
-			while (!preg_match('/[\\s{}()<>\\[\\]]/isu', $oParserState->peek())) {
-				$sResult .= $oParserState->parseCharacter(false);
-			}
-		} else {
-			while (!$oParserState->comes($sQuote)) {
-				$sContent = $oParserState->parseCharacter(false);
-				if ($sContent === null) {
-					throw new SourceException("Non-well-formed quoted string {$oParserState->peek(3)}", $oParserState->currentLine());
-				}
-				$sResult .= $sContent;
-			}
-			$oParserState->consume($sQuote);
-		}
-		return new CSSString($sResult, $oParserState->currentLine());
-	}
+        if ($sBegin === "'") {
+            $sQuote = "'";
+        } elseif ($sBegin === '"') {
+            $sQuote = '"';
+        }
 
-	public function setString($sString) {
-		$this->sString = $sString;
-	}
+        if ($sQuote !== null) {
+            $oParserState->consume($sQuote);
+        }
 
-	public function getString() {
-		return $this->sString;
-	}
+        $sResult = '';
+        $sContent = null;
 
-	public function __toString() {
-		return $this->render(new \Sabberworm\CSS\OutputFormat());
-	}
+        if ($sQuote === null) {
+            // Unquoted strings end in whitespace or with braces, brackets, parentheses
+            while (!preg_match('/[\\s{}()<>\\[\\]]/isu', $oParserState->peek())) {
+                $sResult .= $oParserState->parseCharacter(false);
+            }
+        } else {
+            while (!$oParserState->comes($sQuote)) {
+                $sContent = $oParserState->parseCharacter(false);
 
-	public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat) {
-		$sString = addslashes($this->sString);
-		$sString = str_replace("\n", '\A', $sString);
-		return $oOutputFormat->getStringQuotingType() . $sString . $oOutputFormat->getStringQuotingType();
-	}
+                if ($sContent === null) {
+                    throw new SourceException("Non-well-formed quoted string {$oParserState->peek(3)}", $oParserState->currentLine());
+                }
 
-}
+                $sResult .= $sContent;
+            }
+
+            $oParserState->consume($sQuote);
+        }
+
+        return new self($sResult, $oParserState->currentLine());
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEmpty(): bool
+    {
+        return empty($this->string);
+    }
+
+    /**
+     * @return string
+     */
+    public function getString(): string
+    {
+        return $this->string;
+    }
+
+    /**
+     * @param string $string
+     */
+    public function setString(string $string
