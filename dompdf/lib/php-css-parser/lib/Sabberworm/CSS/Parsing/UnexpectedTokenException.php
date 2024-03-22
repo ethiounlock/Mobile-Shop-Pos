e489e4f62dac760955419921ece4eccb7c0b8e7e
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:33 +0300

    improve

--- dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Parsing/UnexpectedTokenException.php
+++ dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Parsing/UnexpectedTokenException.php
@@ -3,29 +3,45 @@
 namespace Sabberworm\CSS\Parsing;
 
 /**
-* Thrown if the CSS parsers encounters a token it did not expect
-*/
-class UnexpectedTokenException extends SourceException {
-	private $sExpected;
-	private $sFound;
-	// Possible values: literal, identifier, count, expression, search
-	private $sMatchType;
+ * Thrown if the CSS parsers encounters a token it did not expect
+ */
+class UnexpectedTokenException extends SourceException
+{
+    private $sExpected;
+    private $sFound;
+    private $sMatchType;
 
-	public function __construct($sExpected, $sFound, $sMatchType = 'literal', $iLineNo = 0) {
-		$this->sExpected = $sExpected;
-		$this->sFound = $sFound;
-		$this->sMatchType = $sMatchType;
-		$sMessage = "Token “{$sExpected}” ({$sMatchType}) not found. Got “{$sFound}”.";
-		if($this->sMatchType === 'search') {
-			$sMessage = "Search for “{$sExpected}” returned no results. Context: “{$sFound}”.";
-		} else if($this->sMatchType === 'count') {
-			$sMessage = "Next token was expected to have {$sExpected} chars. Context: “{$sFound}”.";
-		} else if($this->sMatchType === 'identifier') {
-			$sMessage = "Identifier expected. Got “{$sFound}”";
-		} else if($this->sMatchType === 'custom') {
-			$sMessage = trim("$sExpected $sFound");
-		}
+    public function __construct(string $sExpected, string $sFound, string $sMatchType = 'literal', int $iLineNo = 0)
+    {
+        $this->sExpected = $sExpected;
+        $this->sFound = $sFound;
+        $this->sMatchType = $sMatchType;
 
-		parent::__construct($sMessage, $iLineNo);
-	}
+        parent::__construct(
+            $this->getMessage($sExpected, $sFound, $sMatchType),
+            $iLineNo
+        );
+    }
+
+    private function getMessage(string $sExpected, string $sFound, string $sMatchType): string
+    {
+        $message = "Token “{$sExpected}” ({$sMatchType}) not found. Got “{$sFound}”.";
+
+        switch ($sMatchType) {
+            case 'search':
+                $message = "Search for “{$sExpected}” returned no results. Context: “{$sFound}”.";
+                break;
+            case 'count':
+                $message = "Next token was expected to have {$sExpected} chars. Context: “{$sFound}”.";
+                break;
+            case 'identifier':
+                $message = "Identifier expected. Got “{$sFound}”";
+                break;
+            case 'custom':
+                $message = trim("$sExpected $sFound");
+                break;
+        }
+
+        return $message;
+    }
 }
