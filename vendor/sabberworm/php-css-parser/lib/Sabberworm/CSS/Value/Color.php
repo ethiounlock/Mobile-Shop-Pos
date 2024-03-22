fd01b7860af0a1a073b6f9d1411a3522ab32dcac
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:07 +0300

    improve

--- vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Value/Color.php
+++ vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Value/Color.php
@@ -3,93 +3,90 @@
 namespace Sabberworm\CSS\Value;
 
 use Sabberworm\CSS\Parsing\ParserState;
+use Sabberworm\CSS\Exception\InvalidCSSException;
 
-class Color extends CSSFunction {
+class Color extends CSSFunction
+{
+    /**
+     * @var array
+     */
+    private $aComponents;
 
-	public function __construct($aColor, $iLineNo = 0) {
-		parent::__construct(implode('', array_keys($aColor)), $aColor, ',', $iLineNo);
-	}
+    /**
+     * Color constructor.
+     * @param array $aColor
+     * @param int $iLineNo
+     */
+    public function __construct(array $aColor, int $iLineNo = 0)
+    {
+        $sName = implode('', array_keys($aColor));
+        parent::__construct($sName, $aColor, ',', $iLineNo);
+        $this->aComponents = $aColor;
+    }
 
-	public static function parse(ParserState $oParserState) {
-		$aColor = array();
-		if ($oParserState->comes('#')) {
-			$oParserState->consume('#');
-			$sValue = $oParserState->parseIdentifier(false);
-			if ($oParserState->strlen($sValue) === 3) {
-				$sValue = $sValue[0] . $sValue[0] . $sValue[1] . $sValue[1] . $sValue[2] . $sValue[2];
-			} else if ($oParserState->strlen($sValue) === 4) {
-				$sValue = $sValue[0] . $sValue[0] . $sValue[1] . $sValue[1] . $sValue[2] . $sValue[2] . $sValue[3] . $sValue[3];
-			}
+    /**
+     * @param ParserState $oParserState
+     * @return static
+     * @throws InvalidCSSException
+     */
+    public static function parse(ParserState $oParserState): self
+    {
+        $aColor = [];
+        if ($oParserState->comes('#')) {
+            $oParserState->consume('#');
+            $sValue = $oParserState->parseIdentifier(false);
+            if ($oParserState->strlen($sValue) === 3 || $oParserState->strlen($sValue) === 4 || $oParserState->strlen($sValue) === 8) {
+                $aColor = self::parseHashColor($sValue);
+            }
+        } else {
+            $sColorMode = $oParserState->parseIdentifier(true);
+            $oParserState->consumeWhiteSpace();
+            $oParserState->consume('(');
+            $iLength = $oParserState->strlen($sColorMode);
+            for ($i = 0; $i < $iLength; ++$i) {
+                $oParserState->consumeWhiteSpace();
+                $aColor[$sColorMode[$i]] = Size::parse($oParserState, true);
+                $oParserState->consumeWhiteSpace();
+                if ($i < ($iLength - 1)) {
+                    $oParserState->consume(',');
+                }
+            }
+            $oParserState->consume(')');
+        }
 
-			if ($oParserState->strlen($sValue) === 8) {
-				$aColor = array(
-					'r' => new Size(intval($sValue[0] . $sValue[1], 16), null, true, $oParserState->currentLine()),
-					'g' => new Size(intval($sValue[2] . $sValue[3], 16), null, true, $oParserState->currentLine()),
-					'b' => new Size(intval($sValue[4] . $sValue[5], 16), null, true, $oParserState->currentLine()),
-					'a' => new Size(round(self::mapRange(intval($sValue[6] . $sValue[7], 16), 0, 255, 0, 1), 2), null, true, $oParserState->currentLine())
-				);
-			} else {
-				$aColor = array(
-					'r' => new Size(intval($sValue[0] . $sValue[1], 16), null, true, $oParserState->currentLine()),
-					'g' => new Size(intval($sValue[2] . $sValue[3], 16), null, true, $oParserState->currentLine()),
-					'b' => new Size(intval($sValue[4] . $sValue[5], 16), null, true, $oParserState->currentLine())
-				);
-			}
-		} else {
-			$sColorMode = $oParserState->parseIdentifier(true);
-			$oParserState->consumeWhiteSpace();
-			$oParserState->consume('(');
-			$iLength = $oParserState->strlen($sColorMode);
-			for ($i = 0; $i < $iLength; ++$i) {
-				$oParserState->consumeWhiteSpace();
-				$aColor[$sColorMode[$i]] = Size::parse($oParserState, true);
-				$oParserState->consumeWhiteSpace();
-				if ($i < ($iLength - 1)) {
-					$oParserState->consume(',');
-				}
-			}
-			$oParserState->consume(')');
-		}
-		return new Color($aColor, $oParserState->currentLine());
-	}
+        if (empty($aColor)) {
+            throw new InvalidCSSException("Invalid color format: {$oParserState->current()}", $oParserState->currentLine());
+        }
 
-	private static function mapRange($fVal, $fFromMin, $fFromMax, $fToMin, $fToMax) {
-		$fFromRange = $fFromMax - $fFromMin;
-		$fToRange = $fToMax - $fToMin;
-		$fMultiplier = $fToRange / $fFromRange;
-		$fNewVal = $fVal - $fFromMin;
-		$fNewVal *= $fMultiplier;
-		return $fNewVal + $fToMin;
-	}
+        return new self($aColor, $oParserState->currentLine());
+    }
 
-	public function getColor() {
-		return $this->aComponents;
-	}
-
-	public function setColor($aColor) {
-		$this->setName(implode('', array_keys($aColor)));
-		$this->aComponents = $aColor;
-	}
-
-	public function getColorDescription() {
-		return $this->getName();
-	}
-
-	public function __toString() {
-		return $this->render(new \Sabberworm\CSS\OutputFormat());
-	}
-
-	public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat) {
-		// Shorthand RGB color values
-		if($oOutputFormat->getRGBHashNotation() && implode('', array_keys($this->aComponents)) === 'rgb') {
-			$sResult = sprintf(
-				'%02x%02x%02x',
-				$this->aComponents['r']->getSize(),
-				$this->aComponents['g']->getSize(),
-				$this->aComponents['b']->getSize()
-			);
-			return '#'.(($sResult[0] == $sResult[1]) && ($sResult[2] == $sResult[3]) && ($sResult[4] == $sResult[5]) ? "$sResult[0]$sResult[2]$sResult[4]" : $sResult);
-		}
-		return parent::render($oOutputFormat);
-	}
-}
+    /**
+     * @param string $sValue
+     * @return array
+     */
+    private static function parseHashColor(string $sValue): array
+    {
+        $aColor = [];
+        $iLength = $oParserState->strlen($sValue);
+        if ($iLength === 3) {
+            $sValue = $sValue[0] . $sValue[0] . $sValue[1] . $sValue[1] . $sValue[2] . $sValue[2];
+            $aColor = [
+                'r' => new Size(hexdec($sValue[0] . $sValue[1]), null, true, $oParserState->currentLine()),
+                'g' => new Size(hexdec($sValue[2] . $sValue[3]), null, true, $oParserState->currentLine()),
+                'b' => new Size(hexdec($sValue[4] . $sValue[5]), null, true, $oParserState->currentLine()),
+            ];
+        } else if ($iLength === 4) {
+            $sValue = $sValue[0] . $sValue[0] . $sValue[1] . $sValue[1] . $sValue[2] . $sValue[2] . $sValue[3] . $sValue[3];
+            $aColor = [
+                'r' => new Size(hexdec($sValue[0] . $sValue[1]), null, true, $oParserState->currentLine()),
+                'g' => new Size(hexdec($sValue[2] . $sValue[3]), null, true, $oParserState->currentLine()),
+                'b' => new Size(hexdec($sValue[4] . $sValue[5]), null, true, $oParserState->currentLine()),
+                'a' => new Size(round(self::mapRange(hexdec($sValue[6] . $sValue[7]), 0, 255, 0, 1), 2), null, true, $oParserState->currentLine()),
+            ];
+        } else if ($iLength === 8) {
+            $aColor = [
+                'r' => new Size(hexdec($sValue[0] . $sValue[1]), null, true, $oParserState->currentLine()),
+                'g' => new Size(hexdec($sValue[2] . $sValue[3]), null, true, $oParserState->currentLine()),
+                'b' => new Size(hexdec($sValue[4] . $sValue[5]), null, true, $oParserState->currentLine()),
+                'a' => new Size(round(self::mapRange(hexdec($sValue[6] . $sValue[7]), 0, 255, 0, 1), 2
