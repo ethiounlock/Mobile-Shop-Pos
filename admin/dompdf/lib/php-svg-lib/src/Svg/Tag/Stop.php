b38563640b6ff39e80c5076c8880918ebe34f66b
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:21 +0300

    improve

--- admin/dompdf/lib/php-svg-lib/src/Svg/Tag/Stop.php
+++ admin/dompdf/lib/php-svg-lib/src/Svg/Tag/Stop.php
@@ -2,7 +2,7 @@
 /**
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
  * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
  */
 
@@ -12,6 +12,28 @@ class Stop extends AbstractTag
 {
     public function start($attributes)
     {
+        // Add a check for required attributes
+        if (!isset($attributes['offset']) || !isset($attributes['style'])) {
+            throw new \Exception('Required attributes "offset" and "style" are missing.');
+        }
 
+        // Add validation for the offset attribute
+        if (filter_var($attributes['offset'], FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^(0|[1-9]\d*)?(\.\d+)?$/']]) === false) {
+            throw new \Exception('The "offset" attribute is not valid.');
+        }
+
+        // Add setting of property values
+        $this->setOffset($attributes['offset']);
+        $this->setStyle($attributes['style']);
     }
-} 
+
+    private function setOffset($offset)
+    {
+        $this->offset = $offset;
+    }
+
+    private function setStyle($style)
+    {
+        $this->style = $style;
+    }
+}
