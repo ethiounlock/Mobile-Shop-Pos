16bb1db7a25951ad657700a897401c8832665e51
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:52 +0300

    improve

--- vendor/dompdf/dompdf/src/Exception/ImageException.php
+++ vendor/dompdf/dompdf/src/Exception/ImageException.php
@@ -1,31 +1,27 @@
 <?php
+
+namespace Dompdf\Exception;
+
 /**
  * @package dompdf
  * @link    http://dompdf.github.com/
  * @author  Benj Carson <benjcarson@digitaljunkies.ca>
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
-namespace Dompdf\Exception;
-
-use Dompdf\Exception;
 
 /**
- * Image exception thrown by DOMPDF
- *
- * @package dompdf
+ * Class ImageException
+ * @package Dompdf\Exception
  */
 class ImageException extends Exception
 {
-
     /**
-     * Class constructor
-     *
-     * @param string $message Error message
-     * @param int $code       Error code
+     * ImageException constructor.
+     * @param string $message
+     * @param int $code
      */
-    function __construct($message = null, $code = 0)
+    public final function __construct(string $message = null, int $code = 0)
     {
         parent::__construct($message, $code);
     }
-
 }
