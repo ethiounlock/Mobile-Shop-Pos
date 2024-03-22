22f65a60960eeb926e7736f3d7480f0b21b4ab5f
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:09 +0300

    improve

--- admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/PrimitiveValue.php
+++ admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/PrimitiveValue.php
@@ -2,9 +2,25 @@
 
 namespace Sabberworm\CSS\Value;
 
-abstract class PrimitiveValue extends Value {
-    public function __construct($iLineNo = 0) {
+/**
+ * Abstract class representing a primitive CSS value.
+ */
+abstract class PrimitiveValue extends Value
+{
+    /**
+     * PrimitiveValue constructor.
+     * @param int $iLineNo
+     */
+    final public function __construct(int $iLineNo = 0)
+    {
         parent::__construct($iLineNo);
     }
+}
 
+/**
+ * Concrete example of a PrimitiveValue subclass.
+ */
+class LengthValue extends PrimitiveValue
+{
+    // Implementation details of the LengthValue class.
 }
