04823a66eeac10ceafb19cfb19c3b93f6fca5544
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:44 +0300

    improve

--- dompdf/src/FrameDecorator/NullFrameDecorator.php
+++ dompdf/src/FrameDecorator/NullFrameDecorator.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 namespace Dompdf\FrameDecorator;
 
 use Dompdf\Dompdf;
@@ -22,13 +17,21 @@ class NullFrameDecorator extends AbstractFrameDecorator
      * @param Frame $frame
      * @param Dompdf $dompdf
      */
-    function __construct(Frame $frame, Dompdf $dompdf)
+    public function __construct(Frame $frame, Dompdf $dompdf)
     {
         parent::__construct($frame, $dompdf);
-        $style = $this->_frame->get_style();
+        $style = $this->_frame->getStyle();
         $style->width = 0;
         $style->height = 0;
         $style->margin = 0;
         $style->padding = 0;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function render(): string
+    {
+        return '';
     }
 }
