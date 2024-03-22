f31eea7b8170a5e1b88d68a39ceb46f2dbd8ab31
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:12 +0300

    improve

--- admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/URL.php
+++ admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/URL.php
@@ -3,47 +3,65 @@
 namespace Sabberworm\CSS\Value;
 
 use Sabberworm\CSS\Parsing\ParserState;
+use Sabberworm\CSS\CSSString;
 
-class URL extends PrimitiveValue {
+/**
+ * URL class representing a CSS url() value.
+ */
+class URL extends PrimitiveValue
+{
+    private $url;
 
-	private $oURL;
+    /**
+     * URL constructor.
+     *
+     * @param CSSString $url URL as a CSSString object
+     * @param int $lineNo Line number for error reporting
+     */
+    public function __construct(CSSString $url, $lineNo = 0)
+    {
+        parent::__construct($lineNo);
+        $this->setURL($url);
+    }
 
-	public function __construct(CSSString $oURL, $iLineNo = 0) {
-		parent::__construct($iLineNo);
-		$this->oURL = $oURL;
-	}
+    /**
+     * Parse a url() value from a ParserState object.
+     *
+     * @param ParserState $oParserState ParserState object
+     * @return URL URL object
+     */
+    public static function parse(ParserState $oParserState)
+    {
+        $bUseUrl = $oParserState->comes('url', true);
+        if ($bUseUrl) {
+            $oParserState->consume('url');
+            $oParserState->consumeWhiteSpace();
+            $oParserState->consume('(');
+        }
+        $oParserState->consumeWhiteSpace();
 
-	public static function parse(ParserState $oParserState) {
-		$bUseUrl = $oParserState->comes('url', true);
-		if ($bUseUrl) {
-			$oParserState->consume('url');
-			$oParserState->consumeWhiteSpace();
-			$oParserState->consume('(');
-		}
-		$oParserState->consumeWhiteSpace();
-		$oResult = new URL(CSSString::parse($oParserState), $oParserState->currentLine());
-		if ($bUseUrl) {
-			$oParserState->consumeWhiteSpace();
-			$oParserState->consume(')');
-		}
-		return $oResult;
-	}
+        // Validate the input as a CSSString
+        $oResult = new URL(CSSString::parse($oParserState), $oParserState->currentLine());
 
+        if ($bUseUrl) {
+            $oParserState->consumeWhiteSpace();
+            $oParserState->consume(')');
+        }
 
-	public function setURL(CSSString $oURL) {
-		$this->oURL = $oURL;
-	}
+        return $oResult;
+    }
 
-	public function getURL() {
-		return $this->oURL;
-	}
+    /**
+     * Set the URL.
+     *
+     * @param CSSString $url URL as a CSSString object
+     */
+    public function setURL(CSSString $url)
+    {
+        $this->url = $url;
+    }
 
-	public function __toString() {
-		return $this->render(new \Sabberworm\CSS\OutputFormat());
-	}
-
-	public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat) {
-		return "url({$this->oURL->render($oOutputFormat)})";
-	}
-
-}
+    /**
+     * Get the URL.
+     *
+     * @return CSSString URL as a CSSString
