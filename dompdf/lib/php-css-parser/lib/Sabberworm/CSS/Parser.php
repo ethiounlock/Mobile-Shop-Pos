3fe5d658b79c38e4eeffaa286ecd2e9e2a3dd77d
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:47 +0300

    improve

--- dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Parser.php
+++ dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Parser.php
@@ -4,38 +4,47 @@ namespace Sabberworm\CSS;
 
 use Sabberworm\CSS\CSSList\Document;
 use Sabberworm\CSS\Parsing\ParserState;
+use Sabberworm\CSS\Settings;
 
 /**
  * Parser class parses CSS from text into a data structure.
  */
-class Parser {
-	private $oParserState;
+class Parser
+{
+    private $oParserState;
 
-	/**
-	 * Parser constructor.
-	 * Note that that iLineNo starts from 1 and not 0
-	 *
-	 * @param $sText
-	 * @param Settings|null $oParserSettings
-	 * @param int $iLineNo
-	 */
-	public function __construct($sText, Settings $oParserSettings = null, $iLineNo = 1) {
-		if ($oParserSettings === null) {
-			$oParserSettings = Settings::create();
-		}
-		$this->oParserState = new ParserState($sText, $oParserSettings, $iLineNo);
-	}
+    /**
+     * Parser constructor.
+     * Note that that iLineNo starts from 1 and not 0
+     *
+     * @param string $sText
+     * @param Settings|null $oParserSettings
+     * @param int $iLineNo
+     */
+    public function __construct(string $sText, Settings $oParserSettings = null, int $iLineNo = 1)
+    {
+        if ($oParserSettings === null) {
+            $oParserSettings = Settings::create();
+        }
+        $this->oParserState = new ParserState($sText, $oParserSettings, $iLineNo);
+    }
 
-	public function setCharset($sCharset) {
-		$this->oParserState->setCharset($sCharset);
-	}
+    /**
+     * Sets the charset for the parser state.
+     *
+     * @param string $sCharset
+     */
+    public function setCharset(string $sCharset): void
+    {
+        $this->oParserState->setCharset($sCharset);
+    }
 
-	public function getCharset() {
-		$this->oParserState->getCharset();
-	}
+    /**
+     * Gets the charset for the parser state.
+     *
+     * @return string
+     */
+    public function getCharset(): string
+    {
+        return $this->oParserState->getCharset();
 
-	public function parse() {
-		return Document::parse($this->oParserState);
-	}
-
-}
