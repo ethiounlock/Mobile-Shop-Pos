75c6d0ed8748a8c1b78fcfbdc497518510b50a0c
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:17 +0300

    improve

--- admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/CSSFunction.php
+++ admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/CSSFunction.php
@@ -2,39 +2,40 @@
 
 namespace Sabberworm\CSS\Value;
 
-class CSSFunction extends ValueList {
+/**
+ * CSSFunction class representing a CSS function.
+ */
+final class CSSFunction extends ValueList {
 
+	/**
+	 * @var string The name of the CSS function.
+	 */
 	protected $sName;
 
-	public function __construct($sName, $aArguments, $sSeparator = ',', $iLineNo = 0) {
-		if($aArguments instanceof RuleValueList) {
-			$sSeparator = $aArguments->getListSeparator();
+	/**
+	 * CSSFunction constructor.
+	 *
+	 * @param string          $sName     The name of the CSS function.
+	 * @param array<Value>   $aArguments The arguments of the CSS function.
+	 * @param int             $iLineNo   The line number where the CSS function is defined.
+	 */
+	final public function __construct(string $sName, array $aArguments, int $iLineNo = 0) {
+		if ($aArguments instanceof RuleValueList) {
 			$aArguments = $aArguments->getListComponents();
 		}
 		$this->sName = $sName;
 		$this->iLineNo = $iLineNo;
-		parent::__construct($aArguments, $sSeparator, $iLineNo);
+		parent::__construct($aArguments, $iLineNo);
 	}
 
-	public function getName() {
+	/**
+	 * Get the name of the CSS function.
+	 *
+	 * @return string The name of the CSS function.
+	 */
+	public function getName(): string {
 		return $this->sName;
 	}
 
-	public function setName($sName) {
-		$this->sName = $sName;
-	}
-
-	public function getArguments() {
-		return $this->aComponents;
-	}
-
-	public function __toString() {
-		return $this->render(new \Sabberworm\CSS\OutputFormat());
-	}
-
-	public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat) {
-		$aArguments = parent::render($oOutputFormat);
-		return "{$this->sName}({$aArguments})";
-	}
-
-}
+	/**
+	 * Set the
