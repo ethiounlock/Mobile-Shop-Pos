a220f1000d93a1e26ba6773fab76159ec4cfd8af
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:10 +0300

    improve

--- vendor/phenx/php-font-lib/src/FontLib/WOFF/File.php
+++ vendor/phenx/php-font-lib/src/FontLib/WOFF/File.php
@@ -1,81 +1,75 @@
 <?php
-/**
- * @package php-font-lib
- * @link    https://github.com/PhenX/php-font-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
 
 namespace FontLib\WOFF;
 
 use FontLib\Table\DirectoryEntry;
+use FontLib\TrueType\File as TrueTypeFile;
 
 /**
  * WOFF font file.
  *
- * @package php-font-lib
- *
- * @property TableDirectoryEntry[] $directory
+ * @property DirectoryEntry[] $directory
  */
-class File extends \FontLib\TrueType\File {
-  function parseHeader() {
-    if (!empty($this->header)) {
-      return;
+class File extends TrueTypeFile
+{
+    public function parseHeader(): void
+    {
+        if (!empty($this->header)) {
+            return;
+        }
+
+        $this->header = new Header($this);
+        $this->header->parse();
     }
 
-    $this->header = new Header($this);
-    $this->header->parse();
-  }
+    public function load(): void
+    {
+        parent::load();
 
-  public function load($file) {
-    parent::load($file);
+        $this->parseTableEntries();
+        $dataOffset = $this->getPos() + count($this->directory) * 20;
 
-    $this->parseTableEntries();
-    $dataOffset = $this->pos() + count($this->directory) * 20;
+        $tempFile = $this->getTempFile(false);
+        $file     = $this->f;
 
-    $fw = $this->getTempFile(false);
-    $fr = $this->f;
+        $this->f = $tempFile;
+        $offset  = $this->header->encode();
 
-    $this->f = $fw;
-    $offset  = $this->header->encode();
+        foreach ($this->directory as $entry) {
+            // Read ...
+            $this->f = $file;
+            $this->seek($entry->offset);
+            $data = $this->read($entry->length);
 
-    foreach ($this->directory as $entry) {
-      // Read ...
-      $this->f = $fr;
-      $this->seek($entry->offset);
-      $data = $this->read($entry->length);
+            if ($entry->origLength && $entry->length < $entry->origLength) {
+                $data = gzdecode($data);
+            }
 
-      if ($entry->length < $entry->origLength) {
-        $data = gzuncompress($data);
-      }
+            // Prepare data ...
+            $length        = strlen($data);
+            $entry->length = $entry->origLength = $length;
+            $entry->offset = $dataOffset;
 
-      // Prepare data ...
-      $length        = strlen($data);
-      $entry->length = $entry->origLength = $length;
-      $entry->offset = $dataOffset;
+            // Write ...
+            $this->f = $tempFile;
 
-      // Write ...
-      $this->f = $fw;
+            // Woff Entry
+            $this->seek($offset);
+            $offset += $this->write( $entry->tag, 4); // tag
+            $offset += $this->writeUInt32($dataOffset); // offset
+            $offset += $this->writeUInt32($length); // length
+            $offset += $this->writeUInt32($length); // origLength
+            $offset += $this->writeUInt32(DirectoryEntry::computeChecksum($data)); // checksum
 
-      // Woff Entry
-      $this->seek($offset);
-      $offset += $this->write($entry->tag, 4); // tag
-      $offset += $this->writeUInt32($dataOffset); // offset
-      $offset += $this->writeUInt32($length); // length
-      $offset += $this->writeUInt32($length); // origLength
-      $offset += $this->writeUInt32(DirectoryEntry::computeChecksum($data)); // checksum
+            // Data
+            $this->seek($dataOffset);
+            $dataOffset += $this->write( $data, $length);
+        }
 
-      // Data
-      $this->seek($dataOffset);
-      $dataOffset += $this->write($data, $length);
-    }
+        $this->f = $tempFile;
+        $this->seek(0);
 
-    $this->f = $fw;
-    $this->seek(0);
+        // Need to re-parse this, don't know why
+        $this->header    = null;
+        $this->directory = array();
 
-    // Need to re-parse this, don't know why
-    $this->header    = null;
-    $this->directory = array();
-    $this->parseTableEntries();
-  }
-}
