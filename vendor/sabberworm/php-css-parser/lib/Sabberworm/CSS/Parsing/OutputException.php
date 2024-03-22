1c61c53da5039147fc376c60e63d14a64836a817
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:11 +0300

    improve

--- vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Parsing/OutputException.php
+++ vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Parsing/OutputException.php
@@ -3,10 +3,14 @@
 namespace Sabberworm\CSS\Parsing;
 
 /**
-* Thrown if the CSS parsers attempts to print something invalid
-*/
-class OutputException extends SourceException {
-	public function __construct($sMessage, $iLineNo = 0) {
-		parent::__construct($sMessage, $iLineNo);
-	}
+ * Thrown if the CSS parsers attempts to print something invalid
+ */
+class OutputException extends SourceException
+{
+    const ERROR_MESSAGE = 'An error occurred while printing CSS output: line %d';
+
+    public function __construct($iLineNo = 0) {
+        $sMessage = sprintf(self::ERROR_MESSAGE, $iLineNo);
+        parent::__construct($sMessage, $iLineNo);
+    }
 }
