95a3a1d56a9fecffcea6e0af4892b088aabdd2b8
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:24 +0300

    improve

--- admin/dompdf/src/FrameDecorator/Image.php
+++ admin/dompdf/src/FrameDecorator/Image.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
+declare(strict_types=1);
+
 namespace Dompdf\FrameDecorator;
 
 use Dompdf\Dompdf;
@@ -19,35 +15,29 @@ use Dompdf\Image\Cache;
  */
 class Image extends AbstractFrameDecorator
 {
-
     /**
-     * The path to the image file (note that remote images are
-     * downloaded locally to Options:tempDir).
-     *
      * @var string
      */
-    protected $_image_url;
+    protected string $_image_url;
 
     /**
-     * The image's file error message
-     *
-     * @var string
+     * @var string|null
      */
-    protected $_image_msg;
+    protected ?string $_image_msg;
 
     /**
      * Class constructor
      *
-     * @param Frame $frame the frame to decorate
-     * @param DOMPDF $dompdf the document's dompdf object (required to resolve relative & remote urls)
+     * @param Frame $frame
+     * @param Dompdf $dompdf
      */
-    function __construct(Frame $frame, Dompdf $dompdf)
+    public function __construct(Frame $frame, Dompdf $dompdf)
     {
         parent::__construct($frame, $dompdf);
+
         $url = $frame->get_node()->getAttribute("src");
 
-        $debug_png = $dompdf->getOptions()->getDebugPng();
-        if ($debug_png) {
+        if ($dompdf->getOptions()->getDebugPng()) {
             print '[__construct ' . $url . ']';
         }
 
@@ -59,9 +49,7 @@ class Image extends AbstractFrameDecorator
             $dompdf
         );
 
-        if (Cache::is_broken($this->_image_url) &&
-            $alt = $frame->get_node()->getAttribute("alt")
-        ) {
+        if (Cache::is_broken($this->_image_url) && $alt = $frame->get_node()->getAttribute("alt")) {
             $style = $frame->get_style();
             $style->width = (4 / 3) * $dompdf->getFontMetrics()->getTextWidth($alt, $style->font_family, $style->font_size, $style->word_spacing);
             $style->height = $dompdf->getFontMetrics()->getFontHeight($style->font_family, $style->font_size);
@@ -73,19 +61,7 @@ class Image extends AbstractFrameDecorator
      *
      * @return string The url of this image
      */
-    function get_image_url()
+    public function get_image_url(): string
     {
         return $this->_image_url;
-    }
 
-    /**
-     * Return the image's error message
-     *
-     * @return string The image's error message
-     */
-    function get_image_msg()
-    {
-        return $this->_image_msg;
-    }
-
-}
