89bc444101020fc6fa0e684e25cec7a48c00fb95
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:53 +0300

    improve

--- vendor/phenx/php-svg-lib/src/autoload.php
+++ vendor/phenx/php-svg-lib/src/autoload.php
@@ -1,17 +1,21 @@
 <?php
+
 /**
+ * Autoloader for the php-svg-lib library
+ *
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
  * @author  Fabien Ménager <fabien.menager@gmail.com>
  * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
  */
 
-spl_autoload_register(function($class) {
-  if (0 === strpos($class, "Svg")) {
-    $file = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $file = realpath(__DIR__ . DIRECTORY_SEPARATOR . $file . '.php');
-    if (file_exists($file)) {
-      include_once $file;
+spl_autoload_register(function ($class) {
+    if (strpos($class, 'Svg') === 0) {
+        $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+        $file = realpath(__DIR__ . DIRECTORY_SEPARATOR . $file);
+
+        if (file_exists($file)) {
+            include $file;
+        }
     }
-  }
 });
