b9f0db488ea447de21ba6fc656c6e2cea9b82ac7
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:20 +0300

    improve

--- admin/dompdf/src/FrameReflower/ListBullet.php
+++ admin/dompdf/src/FrameReflower/ListBullet.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 namespace Dompdf\FrameReflower;
 
 use Dompdf\FrameDecorator\Block as BlockFrameDecorator;
@@ -12,8 +7,6 @@ use Dompdf\FrameDecorator\AbstractFrameDecorator;
 
 /**
  * Reflows list bullets
- *
- * @package dompdf
  */
 class ListBullet extends AbstractFrameReflower
 {
@@ -22,15 +15,16 @@ class ListBullet extends AbstractFrameReflower
      * ListBullet constructor.
      * @param AbstractFrameDecorator $frame
      */
-    function __construct(AbstractFrameDecorator $frame)
+    public function __construct(AbstractFrameDecorator $frame)
     {
         parent::__construct($frame);
     }
 
     /**
      * @param BlockFrameDecorator|null $block
+     * @return ListBullet
      */
-    function reflow(BlockFrameDecorator $block = null)
+    public function reflow(?BlockFrameDecorator $block = null): ListBullet
     {
         $style = $this->_frame->get_style();
 
@@ -41,5 +35,7 @@ class ListBullet extends AbstractFrameReflower
             $p = $this->_frame->find_block_parent();
             $p->add_frame_to_line($this->_frame);
         }
+
+        return $this;
     }
 }
