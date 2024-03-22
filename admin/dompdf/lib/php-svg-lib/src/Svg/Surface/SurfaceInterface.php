ddcf01fb936b36ac93524c19d2d855f2943f5137
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:22 +0300

    improve

--- admin/dompdf/lib/php-svg-lib/src/Svg/Surface/SurfaceInterface.php
+++ admin/dompdf/lib/php-svg-lib/src/Svg/Surface/SurfaceInterface.php
@@ -1,17 +1,16 @@
 <?php
-/**
- * @package php-svg-lib
- * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien M�nager <fabien.menager@gmail.com>
- * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
- */
+
+declare(strict_types=1);
 
 namespace Svg\Surface;
 
 use Svg\Style;
 
+const FILL_RULE_NONZERO = 'nonzero';
+const FILL_RULE_EVENODD = 'evenodd';
+
 /**
- * Interface Surface, like CanvasRenderingContext2D
+ * Interface SurfaceInterface, like CanvasRenderingContext2D
  *
  * @package Svg
  */
@@ -22,69 +21,93 @@ interface SurfaceInterface
     public function restore();
 
     // transformations (default transform is the identity matrix)
-    public function scale($x, $y);
+    public function scale(float $x, float $y): void;
 
-    public function rotate($angle);
+    public function rotate(float $angle): void;
 
-    public function translate($x, $y);
+    public function translate(float $x, float $y): void;
 
-    public function transform($a, $b, $c, $d, $e, $f);
+    public function transform(
+        float $a,
+        float $b,
+        float $c,
+        float $d,
+        float $e,
+        float $f
+    ): void;
 
     // path ends
-    public function beginPath();
+    public function beginPath(): void;
 
-    public function closePath();
+    public function closePath(): void;
 
-    public function fill();
+    public function fill(string $fillRule = FILL_RULE_NONZERO): void;
 
-    public function stroke();
+    public function stroke(): void;
 
-    public function endPath();
+    public function endPath(): void;
 
-    public function fillStroke();
+    public function fillStroke(): void;
 
-    public function clip();
+    public function clip(): void;
 
     // text (see also the CanvasDrawingStyles interface)
-    public function fillText($text, $x, $y, $maxWidth = null);
+    public function fillText(
+        string $text,
+        float $x,
+        float $y,
+        ?float $maxWidth = null
+    ): void;
 
-    public function strokeText($text, $x, $y, $maxWidth = null);
+    public function strokeText(
+        string $text,
+        float $x,
+        float $y,
+        ?float $maxWidth = null
+    ): void;
 
-    public function measureText($text);
+    public function measureText(string $text): array;
 
     // drawing images
-    public function drawImage($image, $sx, $sy, $sw = null, $sh = null, $dx = null, $dy = null, $dw = null, $dh = null);
+    public function drawImage(
+        $image,
+        float $sx,
+        float $sy,
+        ?float $sw = null,
+        ?float $sh = null,
+        float $dx,
+        float $dy,
+        ?float $dw = null,
+        ?float $dh = null
+    ): void;
 
     // paths
-    public function lineTo($x, $y);
+    public function lineTo(float $x, float $y): void;
 
-    public function moveTo($x, $y);
+    public function moveTo(float $x, float $y): void;
 
-    public function quadraticCurveTo($cpx, $cpy, $x, $y);
+    public function quadraticCurveTo(float $cpx, float $cpy, float $x, float $y): void;
 
-    public function bezierCurveTo($cp1x, $cp1y, $cp2x, $cp2y, $x, $y);
+    public function bezierCurveTo(
+        float $cp1x,
+        float $cp1y,
+        float $cp2x,
+        float $cp2y,
+        float $x,
+        float $y
+    ): void;
 
-    public function arcTo($x1, $y1, $x2, $y2, $radius);
+    public function arcTo(
+        float $x1,
+        float $y1,
+        float $x2,
+        float $y2,
+        float $radius
+    ): void;
 
-    public function circle($x, $y, $radius);
+    public function circle(float $x, float $y, float $radius): void;
 
-    public function arc($x, $y, $radius, $startAngle, $endAngle, $anticlockwise = false);
-
-    public function ellipse($x, $y, $radiusX, $radiusY, $rotation, $startAngle, $endAngle, $anticlockwise);
-
-    // Rectangle
-    public function rect($x, $y, $w, $h, $rx = 0, $ry = 0);
-
-    public function fillRect($x, $y, $w, $h);
-
-    public function strokeRect($x, $y, $w, $h);
-
-    public function setStyle(Style $style);
-
-    /**
-     * @return Style
-     */
-    public function getStyle();
-
-    public function setFont($family, $style, $weight);
-}
+    public function arc(
+        float $x,
+        float $y,
+        float $
