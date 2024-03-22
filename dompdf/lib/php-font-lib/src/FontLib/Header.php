25a2597b70ef383d21e315a23214b8e999f88b83
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:40 +0300

    improve

--- dompdf/lib/php-font-lib/src/FontLib/Header.php
+++ dompdf/lib/php-font-lib/src/FontLib/Header.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @package php-font-lib
- * @link    https://github.com/PhenX/php-font-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 namespace FontLib;
 
 use FontLib\TrueType\File;
@@ -14,24 +9,37 @@ use FontLib\TrueType\File;
  *
  * @package php-font-lib
  */
-abstract class Header extends BinaryStream {
-  /**
-   * @var File
-   */
-  protected $font;
-  protected $def = array();
+abstract class Header extends BinaryStream
+{
+    /**
+     * @var File
+     */
+    protected $font;
 
-  public $data;
+    protected string $data;
 
-  public function __construct(File $font) {
-    $this->font = $font;
-  }
+    public function __construct(File $font)
+    {
+        $this->font = $font;
+    }
 
-  public function encode() {
-    return $this->font->pack($this->def, $this->data);
-  }
+    abstract protected function getDef(): array;
 
-  public function parse() {
-    $this->data = $this->font->unpack($this->def);
-  }
+    public function encode(): ?string
+    {
+        if (!$this->data) {
+            return null;
+        }
+
+        return $this->font->pack($this->getDef(), $this->data);
+    }
+
+    public function parse(): void
+    {
+        $this->data = $this->font->unpack($this->getDef());
+
+        if (empty($this->data)) {
+            throw new \RuntimeException('Failed to parse header data');
+        }
+    }
 }
