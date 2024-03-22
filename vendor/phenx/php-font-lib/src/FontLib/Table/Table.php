adf5bcba7bd420ab3e149e272b698fb7eb339dba
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:13 +0300

    improve

--- vendor/phenx/php-font-lib/src/FontLib/Table/Table.php
+++ vendor/phenx/php-font-lib/src/FontLib/Table/Table.php
@@ -1,14 +1,11 @@
 <?php
-/**
- * @package php-font-lib
- * @link    https://github.com/PhenX/php-font-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
+declare(strict_types=1);
+
 namespace FontLib\Table;
 
-use FontLib\TrueType\File;
 use FontLib\Font;
+use FontLib\TrueType\File;
 use FontLib\BinaryStream;
 
 /**
@@ -16,78 +13,85 @@ use FontLib\BinaryStream;
  *
  * @package php-font-lib
  */
-class Table extends BinaryStream {
-  /**
-   * @var DirectoryEntry
-   */
-  protected $entry;
-  protected $def = array();
+class Table extends BinaryStream
+{
+    /**
+     * @var DirectoryEntry
+     */
+    protected $entry;
 
-  public $data;
+    protected array $def = [];
 
-  final public function __construct(DirectoryEntry $entry) {
-    $this->entry = $entry;
-    $entry->setTable($this);
-  }
+    public $data;
 
-  /**
-   * @return File
-   */
-  public function getFont() {
-    return $this->entry->getFont();
-  }
-
-  protected function _encode() {
-    if (empty($this->data)) {
-      Font::d("  >> Table is empty");
-
-      return 0;
+    /**
+     * Table constructor.
+     *
+     * @param DirectoryEntry $entry
+     */
+    final public function __construct(DirectoryEntry $entry)
+    {
+        $this->entry = $entry;
+        $entry->setTable($this);
+        $this->def = $this->getDefinition();
     }
 
-    return $this->getFont()->pack($this->def, $this->data);
-  }
-
-  protected function _parse() {
-    $this->data = $this->getFont()->unpack($this->def);
-  }
-
-  protected function _parseRaw() {
-    $this->data = $this->getFont()->read($this->entry->length);
-  }
-
-  protected function _encodeRaw() {
-    return $this->getFont()->write($this->data, $this->entry->length);
-  }
-
-  public function toHTML() {
-    return "<pre>" . var_export($this->data, true) . "</pre>";
-  }
-
-  final public function encode() {
-    $this->entry->startWrite();
-
-    if (false && empty($this->def)) {
-      $length = $this->_encodeRaw();
-    }
-    else {
-      $length = $this->_encode();
+    /**
+     * @return File
+     */
+    final public function getFont(): Font
+    {
+        return $this->entry->getFont() ?? throw new \RuntimeException('Font object is not set');
     }
 
-    $this->entry->endWrite();
+    /**
+     * @return array
+     */
+    abstract protected function getDefinition(): array;
 
-    return $length;
-  }
+    protected function _encode(): int
+    {
+        if (empty($this->data)) {
+            Font::d('  >> Table is empty');
 
-  final public function parse() {
-    $this->entry->startRead();
+            return 0;
+        }
 
-    if (false && empty($this->def)) {
-      $this->_parseRaw();
+        return $this->getFont()->pack($this->def, $this->data);
     }
-    else {
-      $this->_parse();
+
+    protected function _parse(): void
+    {
+        $this->data = $this->getFont()->unpack($this->def);
     }
 
-    $this->entry->endRead();
-  }
-}
+    protected function _parseRaw(): void
+    {
+        $this->data = $this->getFont()->read($this->entry->length);
+    }
+
+    protected function _encodeRaw(): string
+    {
+        return $this->getFont()->write($this->data, $this->entry->length);
+    }
+
+    final public function encode(): int
+    {
+        $this->entry->startWrite();
+
+        if (false && empty($this->def)) {
+            $length = $this->_encodeRaw();
+        } else {
+            $length = $this->_encode();
+        }
+
+        $this->entry->endWrite();
+
+        return $length;
+    }
+
+    final public function parse(): void
+    {
+        $this->entry->startRead();
+
+        if (false && empty($this->def
