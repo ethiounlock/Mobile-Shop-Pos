23ff7526908ed677cb6b371efac60d59d80747c4
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:44 +0300

    improve

--- change_password.php
+++ change_password.php
@@ -1,60 +1,49 @@
-
-
 <?php
-   include 'database_connection.php';
-    $error="";
-if ($_POST)
-{   
-    
-    $name=$_POST['usr'];
-    $pswd=$_POST['pass'];
-    $pswd1=$_POST['pass_1'];
-    $pswd2=$_POST['pass_2'];
-    
-    
-        
-    if($pswd1 == $pswd2)
-        {
-            
-            $query = mysqli_query($conn,"SELECT * FROM login_tbl WHERE user_name='$name' AND usr_password='$pswd' AND category='Employee'"); // selecting data through mysql_query()
-            $rowquery = mysqli_num_rows($query);
-            if ($rowquery == 1)
-            {
-                $query1=mysqli_query($conn,"UPDATE login_tbl SET usr_password='$pswd1' WHERE user_name='$name' AND usr_password='$pswd' AND category='Employee'");
-                echo'<script>';
-                echo'window.prompt("Successfully Changed password")';
-                
-                 echo'</script>';
-                 
-                 
+// Include the database connection file
+include 'database_connection.php';
 
-                
+// Initialize error variable
+$error = "";
 
-               
+// Check if form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST['usr'];
+    $currentPassword = $_POST['pass'];
+    $newPassword = $_POST['pass_1'];
+    $confirmPassword = $_POST['pass_2'];
+
+    // Check if new passwords match
+    if ($newPassword === $confirmPassword) {
+        // Get user data from database
+        $query = mysqli_query($conn, "SELECT * FROM login_tbl WHERE user_name='$name' AND usr_password='$currentPassword' AND category='Employee'");
+        $rowquery = mysqli_num_rows($query);
+
+        // Check if user exists
+        if ($rowquery == 1) {
+            // Update user password in database
+            $query1 = mysqli_query($conn, "UPDATE login_tbl SET usr_password='$newPassword' WHERE user_name='$name' AND usr_password='$currentPassword' AND category='Employee'");
+
+            // Check if password was updated successfully
+            if ($query1) {
+                echo '<script>';
+                echo 'window.prompt("Successfully Changed password")';
+                echo '</script>';
+            } else {
+                $error = "Error changing password. Please try again.";
             }
-            else
-            {
-                
-                $error1="Invalid username and password";
-                
-            }
+        } else {
+            $error = "Invalid username or current password.";
         }
-        else
-        {
-            $error="Password mismatch!";
-            
-        }
-        
+    } else {
+        $error = "New passwords do not match.";
     }
+}
 ?>
 
-
-
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-  
-
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="description" content="Fashi Template">
     <meta name="keywords" content="Fashi, unica, creative, html">
@@ -82,222 +71,53 @@ if ($_POST)
     <meta name="robots" content="noindex, nofollow">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
-
-    <!-- Latest compiled and minified CSS -->
-
-
-
     <!-- jQuery library -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-
-
-
-    
-
-    <script src="js/jquery.min.js"></script>
-    <!--<script src="js/bootstrap.min.js"></script> -->
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" > -->
-    
-
+    <!-- DataTables CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
-  
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
-  
- <!-- https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css
-https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap5.min.css -->
 
+    <!-- DataTables Bootstrap 5 CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap5.min.css">
+
+    <!-- DataTables JS -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
+
+    <!-- DataTables Bootstrap 5 JS -->
     <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap5.min.js"></script>
-    
-    
-
 
     <style>
-/* Remove the navbar's default margin-bottom and rounded borders */ 
+        /* Remove the navbar's default margin-bottom and rounded borders */
+        .navbar {
+            margin-bottom: 4px;
+            border-radius: 0;
+        }
 
-.navbar {
-      margin-bottom: 4px;
-      border-radius: 0;
-      }
-      /* Add a gray background color and some padding to the footer */
-      footer {
-      background-color: #f2f2f2;
-      padding: 25px;
-      }
-      .carousel-inner img {
-      width: 100%; /* Set width to 100% */
-      margin: auto;
-      min-height:200px;
-      }
-      .navbar-brand
-      {
-      padding:5px 40px;
-      }
-      .navbar-brand:hover
-      {
-      background-color:#ffffff;
-      }
-      /* Hide the carousel text when the screen is less than 600 pixels wide */
-      @media (max-width: 600px) {
-      .carousel-caption {
-      display: none; 
-      }
-      }
-    </style>
+        /* Add a gray background color and some padding to the footer */
+        footer {
+            background-color: #f2f2f2;
+            padding: 25px;
+        }
 
-<body>
-    <!-- Page Preloder -->
+        .carousel-inner img {
+            width: 100%;
+            /* Set width to 100% */
+            margin: auto;
+            min-height: 200px;
+        }
 
+        .navbar-brand {
+            padding: 5px 40px;
+        }
 
-    <!-- Header Section Begin -->
-    
-    <header class="header-section">
+        .navbar-brand:hover {
+            background-color: #ffffff;
+        }
 
-<div class="container">
-    <div class="inner-header">
-        <div class="row">
-            <div class="col-lg-2 col-md-2">
-                <!-- mobile shop logo -->
-                
-
-
-        </div>
-    </div>
-</div>
-<!-- menu section begins -->
-<div class="nav-item">
-    <div class="container">
-        <div class="nav-depart">
-
-        </div>
-        <nav class="nav-menu mobile-menu">
-            <ul>
-                <li class="active"></li>
-
-
-                        <li><a href="./add_product.php">Add Product</a></li>
-                        <li><a href="./view_product.php">View Product</a></li>
-                        <li><a href="./view_purchase.php">Purchase History</a></li>
-                <li><a href="./invoice.php">Sale</a></li>
-                <li><a href="#">Report</a>
-                  <ul class="dropdown">
-                    <li><a href="./sales_report.php">Sales Report</a></li>
-                    <li><a href="./purchase_report.php">Purchase Report</a></li>
-                      </ul>
-                        
-                        
-
-
-                </li>
-                <li><a href="#">Profile</a>
-      <ul class="dropdown">
-            <li><a href="./change_username.php">Change username</a></li>
-            <li><a href="./change_password.php">Change password</a></li>
-            <li><a href="./logout.php">Log out</a></li>
-              </ul>
-              </li>
-
-
-
-
-        </nav>
-    </div>
-</div>
-        <div id="mobile-menu-wrap"></div>
-
-
-</header>
-<!-- here we can enter the new password and go for submit -->  
-
-<head>
-<meta charset="UTF-8">
-<style>
-@font-face {
-  font-family: OpenSans-Regular;
-  src: url('../fonts/OpenSans/OpenSans-Regular.ttf'); 
-}
-.header
-{ 
-   width: 94.75%;
-	  height:10%;
-    font-family: OpenSans-Regular;
-    
-	  color:#fff;
-	  background-color:#00264d;
-    border:2px solid #3399ff;
-	  padding:2.5%;
-}
-#form {
-     text-align:center;
-     margin-left:25%;
-     margin-right:5%;
-     margin-top:5%;
-     font-family: OpenSans-Regular, sans-serif;
-     padding:5%;
-
-     width:50%;
-     background: #fff;
-     border-radius: 10px;
-     overflow: hidden;
-     box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
-     -moz-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
-     -webkit-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
-     -o-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
-     -ms-box-shadow: 0 3px 20px 0px rgba(0, 0, 0, 0.1);
-     
-     }
-     #sline{background-color: #fff;
-            border: 1px solid #ccd8ff;}
-     #submit {
-         margin-left:40%;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -moz-box;
-  display: -ms-flexbox;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0px;
-  width: 20%;
-  height:5%;
-  background-color:  #00cc7a;
-  font-family: OpenSans-Regular;
-  font-size: 14px;
-  color: #fff;
-  line-height: 1.2;
-  text-transform: uppercase;
-  -webkit-transition: all 0.4s;
-  -o-transition: all 0.4s;
-  -moz-transition: all 0.4s;
-  transition: all 0.4s;
-  }
-  .submit:hover {
-  background-color: #333333;
-}
-
-</style>
-<title>register</title>
-</head>
-<body  style="background-color:#d9d9d9";>
-
-
-<?php
-
-echo "<div id='form'>";
-echo "<form method='post' >";//may need to change get to post
-?>
-    <p style="color:red;"><i><?php echo $error; ?></i></p>
-    
-    Enter  username &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input id='sline' type="text" name="usr" required><br><br>
-    Enter the password &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input id='sline' type="password" name="pass" required><br><br>
-	Enter new password &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input id='sline' type="password" name="pass_1" required><br><br>
-	Confirm new password &nbsp;<input  id='sline' type="password" name="pass_2" required><br><br>
-    
-    <br><br><input  id='submit' type='submit' value='submit' >
-
-</form>
-</div>
-</body>
-</html>
+        /* Hide the carousel text when the screen is less than 600 pixels
