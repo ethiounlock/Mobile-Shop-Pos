38387b2fdcbc9a63e852c98296e97fc0d826cc0a
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:27 +0300

    improve

--- admin/dompdf/lib/php-font-lib/src/FontLib/TrueType/TableDirectoryEntry.php
+++ admin/dompdf/lib/php-font-lib/src/FontLib/TrueType/TableDirectoryEntry.php
@@ -1,33 +1,47 @@
 <?php
-/**
- * @package php-font-lib
- * @link    https://github.com/PhenX/php-font-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
 
 namespace FontLib\TrueType;
 
 use FontLib\Table\DirectoryEntry;
+use FontLib\Font;
 
 /**
  * TrueType table directory entry.
  *
  * @package php-font-lib
  */
-class TableDirectoryEntry extends DirectoryEntry {
-  function __construct(File $font) {
-    parent::__construct($font);
-  }
+class TableDirectoryEntry extends DirectoryEntry
+{
+    /**
+     * @var Font The parent Font object.
+     */
+    protected Font $font;
 
-  function parse() {
-    parent::parse();
+    /**
+     * TableDirectoryEntry constructor.
+     *
+     * @param Font $font The parent Font object.
+     */
+    public function __construct(Font $font)
+    {
+        parent::__construct($font);
+        $this->font = $font;
+    }
 
-    $font           = $this->font;
-    $this->checksum = $font->readUInt32();
-    $this->offset   = $font->readUInt32();
-    $this->length   = $font->readUInt32();
-    $this->entryLength += 12;
-  }
+    /**
+     * Parse the table directory entry.
+     */
+    public function parse(): void
+    {
+        parent::parse();
+
+        $this->checksum = $this->font->readUInt32();
+        $this->offset = $this->font->readUInt32();
+        $this->length = $this->font->readUInt32();
+
+        // Added error handling for reading table data in case the file pointer is at the end of the file.
+        if ($this->offset + $this->length > $this->font->getFileSize()) {
+            throw new \RuntimeException('Invalid table directory entry.');
+        }
+    }
 }
-
