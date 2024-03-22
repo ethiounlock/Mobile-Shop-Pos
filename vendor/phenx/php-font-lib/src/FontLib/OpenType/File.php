71077c84faaeb660fa575b49b250027dfbb224fa
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:57 +0300

    improve

--- vendor/phenx/php-font-lib/src/FontLib/OpenType/File.php
+++ vendor/phenx/php-font-lib/src/FontLib/OpenType/File.php
@@ -8,11 +8,36 @@
 
 namespace FontLib\OpenType;
 
+use FontLib\TrueType\File as TrueTypeFile;
+
 /**
  * Open Type font, the same as a TrueType one.
  *
+ * This class represents OpenType font files, which are a superset of TrueType fonts.
+ * It extends the `\FontLib\TrueType\File` class to provide additional functionality
+ * specific to OpenType fonts.
+ *
  * @package php-font-lib
  */
-class File extends \FontLib\TrueType\File {
-  //
+class File extends TrueTypeFile {
+  const FILE_FORMAT_OPEN_TYPE = 'OpenType';
+
+  /**
+   * Create a new OpenType font file.
+   *
+   * @param string $filename The path to the font file.
+   */
+  public function __construct($filename) {
+    parent::__construct($filename);
+    $this->file_format = self::FILE_FORMAT_OPEN_TYPE;
+  }
+
+  /**
+   * Get the name of the file format.
+   *
+   * @return string The name of the file format.
+   */
+  public function getFormatName() {
+    return $this->file_format;
+  }
 }
