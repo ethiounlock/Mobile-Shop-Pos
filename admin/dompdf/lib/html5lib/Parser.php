6b8d394761e45b206dc6c8134ab9cad571e32b2b
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:08 +0300

    improve

--- admin/dompdf/lib/html5lib/Parser.php
+++ admin/dompdf/lib/html5lib/Parser.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once dirname(__FILE__) . '/Data.php';
-require_once dirname(__FILE__) . '/InputStream.php';
-require_once dirname(__FILE__) . '/TreeBuilder.php';
-require_once dirname(__FILE__) . '/Tokenizer.php';
+declare(strict_types=1);
+
+require_once dirname(__FILE__) ?? '/' . 'Data.php';
+require_once dirname(__FILE__) ?? '/' . 'InputStream.php';
+require_once dirname(__FILE__) ?? '/' . 'TreeBuilder.php';
+require_once dirname(__FILE__) ?? '/' . 'Tokenizer.php';
 
 /**
  * Outwards facing interface for HTML5.
@@ -12,11 +14,13 @@ class HTML5_Parser
 {
     /**
      * Parses a full HTML document.
-     * @param $text | HTML text to parse
-     * @param $builder | Custom builder implementation
-     * @return DOMDocument|DOMNodeList Parsed HTML as DOMDocument
+     *
+     * @param string $text HTML text to parse
+     * @param HTML5_TreeBuilder|null $builder Custom builder implementation
+     * @return \DOMDocument|\DOMNodeList Parsed HTML as DOMDocument
      */
-    public static function parse($text, $builder = null) {
+    public static function parse(string $text, ?HTML5_TreeBuilder $builder = null): \DOMDocument|\DOMNodeList
+    {
         $tokenizer = new HTML5_Tokenizer($text, $builder);
         $tokenizer->parse();
         return $tokenizer->save();
@@ -24,14 +28,10 @@ class HTML5_Parser
 
     /**
      * Parses an HTML fragment.
-     * @param $text | HTML text to parse
-     * @param $context String name of context element to pretend parsing is in.
-     * @param $builder | Custom builder implementation
-     * @return DOMDocument|DOMNodeList Parsed HTML as DOMDocument
+     *
+     * @param string $text HTML text to parse
+     * @param string|null $context String name of context element to pretend parsing is in.
+     * @param HTML5_TreeBuilder|null $builder Custom builder implementation
+     * @return \DOMDocument|\DOMNodeList Parsed HTML as DOMDocument
      */
-    public static function parseFragment($text, $context = null, $builder = null) {
-        $tokenizer = new HTML5_Tokenizer($text, $builder);
-        $tokenizer->parseFragment($context);
-        return $tokenizer->save();
-    }
-}
+    public static function parseFragment(string $text, ?string $context = null, ?HTML5_Tree
