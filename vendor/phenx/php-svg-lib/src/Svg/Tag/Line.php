5b2df445cea9905bccaf11b0a97860df7d5fbe98
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:17 +0300

    improve

--- vendor/phenx/php-svg-lib/src/Svg/Tag/Line.php
+++ vendor/phenx/php-svg-lib/src/Svg/Tag/Line.php
@@ -1,38 +1,31 @@
 <?php
-/**
- * @package php-svg-lib
- * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
- */
 
 namespace Svg\Tag;
 
+/**
+ * @package php-svg-lib
+ * @link    http://github.com/PhenX/php-svg-lib
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
+ * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
+ */
+
 class Line extends Shape
 {
-    protected $x1 = 0;
-    protected $y1 = 0;
+    protected int $x1 = 0;
+    protected int $y1 = 0;
 
-    protected $x2 = 0;
-    protected $y2 = 0;
+    protected int $x2 = 0;
+    protected int $y2 = 0;
 
-    public function start($attributes)
+    public function start(array $attributes): void
     {
-        if (isset($attributes['x1'])) {
-            $this->x1 = $attributes['x1'];
-        }
-        if (isset($attributes['y1'])) {
-            $this->y1 = $attributes['y1'];
-        }
-        if (isset($attributes['x2'])) {
-            $this->x2 = $attributes['x2'];
-        }
-        if (isset($attributes['y2'])) {
-            $this->y2 = $attributes['y2'];
-        }
+        $this->x1 = $attributes['x1'] ?? 0;
+        $this->y1 = $attributes['y1'] ?? 0;
+        $this->x2 = $attributes['x2'] ?? 0;
+        $this->y2 = $attributes['y2'] ?? 0;
 
         $surface = $this->document->getSurface();
         $surface->moveTo($this->x1, $this->y1);
         $surface->lineTo($this->x2, $this->y2);
     }
-} 
+}
