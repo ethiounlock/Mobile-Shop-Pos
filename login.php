1032d31cc95b432f8530ead5a0c998152e163780
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:02 +0300

    improve

--- login.php
+++ login.php
@@ -1,43 +1,42 @@
 <?php
-    include 'database_connection.php';
-      if ($_POST){
-      $user=$_POST['username'];
-      $pass=$_POST['password'];
-      $cat=$_POST['login'];
-      
-      
-if($cat == 'Employee')
-{
+// Include database connection file
+include 'database_connection.php';
 
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get input from form
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+    $cat = $_POST['login'];
 
- $query = mysqli_query($conn, "SELECT * FROM login_tbl WHERE (usr_password='$pass' AND user_name='$user') AND category='Employee'");
+    // Sanitize input to prevent SQL injection
+    $user = stripslashes($user);
+    $pass = stripslashes($pass);
+    $user = mysqli_real_escape_string($conn, $user);
+    $pass = mysqli_real_escape_string($conn, $pass);
 
- $rows = mysqli_num_rows($query);
- if($rows == 1){
- header("Location: add_product.php"); // Redirecting to other page
- }
- else
- {
- header("Location: invalid_login.html");
- }
-} 
-else
-{
-    $query = mysqli_query($conn, "SELECT * FROM login_tbl WHERE (usr_password='$pass' AND user_name='$user') AND category='Admin'");
+    // Query database based on category
+    if ($cat == 'Employee') {
+        $query = mysqli_query($conn, "SELECT * FROM login_tbl WHERE user_name='$user' AND usr_password='$pass' AND category='Employee'");
+    } else {
+        $query = mysqli_query($conn, "SELECT * FROM login_tbl WHERE user_name='$user' AND usr_password='$pass' AND category='Admin'");
+    }
 
-    
- $rows = mysqli_num_rows($query);
- if($rows == 1){
- header("Location: admin/admin_employee.php"); // Redirecting to admin  page
- }
- else
- {
- header("Location: invalid_login.html");
- }
+    // Check if query returns a result
+    if (mysqli_num_rows($query) == 1) {
+        // Redirect to appropriate page based on category
+        if ($cat == 'Employee') {
+            header("Location: add_product.php");
+        } else {
+            header("Location: admin/admin_employee.php");
+        }
+    } else {
+        // Redirect to invalid login page
+        header("Location: invalid_login.html");
+    }
 
-} 
- mysqli_close($conn); // Closing connection
+    // Close database connection
+    mysqli_close($conn);
 
 }
-    
 ?>
