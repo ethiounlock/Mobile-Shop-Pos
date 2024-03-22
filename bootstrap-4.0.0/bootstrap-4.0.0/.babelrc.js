ca7229d39bf55230da385500eeafb460b361a807
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:28 +0300

    improve

--- bootstrap-4.0.0/bootstrap-4.0.0/.babelrc.js
+++ bootstrap-4.0.0/bootstrap-4.0.0/.babelrc.js
@@ -1,16 +1,13 @@
+const plugins =
+  process.env.PLUGINS && ['transform-es2015-modules-strip', '@babel/proposal-object-rest-spread']
+
 module.exports = {
   presets: [
-    [
-      '@babel/env',
-      {
-        loose: true,
-        modules: false,
-        exclude: ['transform-typeof-symbol']
-      }
-    ]
+    ['@babel/env', {
+      loose: true,
+      modules: false,
+      exclude: ['transform-typeof-symbol']
+    }]
   ],
-  plugins: [
-    process.env.PLUGINS && 'transform-es2015-modules-strip',
-    '@babel/proposal-object-rest-spread'
-  ].filter(Boolean)
+  plugins: plugins.filter(Boolean)
 };
