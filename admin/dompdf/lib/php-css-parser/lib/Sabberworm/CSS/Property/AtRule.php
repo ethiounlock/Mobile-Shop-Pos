89cbbe7aa9045939ce8946f2fd1079a2e92fe908
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:08 +0300

    improve

--- admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Property/AtRule.php
+++ admin/dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Property/AtRule.php
@@ -4,13 +4,21 @@ namespace Sabberworm\CSS\Property;
 
 use Sabberworm\CSS\Renderable;
 use Sabberworm\CSS\Comment\Commentable;
+use Sabberworm\CSS\Exception\InvalidArgumentException;
 
-interface AtRule extends Renderable, Commentable {
-	// Since there are more set rules than block rules, we’re whitelisting the block rules and have anything else be treated as a set rule.
-	const BLOCK_RULES = 'media/document/supports/region-style/font-feature-values';
-	// …and more font-specific ones (to be used inside font-feature-values)
-	const SET_RULES = 'font-face/counter-style/page/swash/styleset/annotation';
-	
-	public function atRuleName();
-	public function atRuleArgs();
-}
+interface AtRule extends Renderable, Commentable
+{
+    // Since there are more set rules than block rules, we’re whitelisting the block rules and have anything else be treated as a set rule.
+    const BLOCK_RULES = 'media, document, supplies, region-style, font-feature-values';
+    // …and more font-specific ones (to be used inside font-feature-values)
+    const SET_RULES = 'font-face, counter-style, page, swash, styleset, annotation';
+
+    /**
+     * @return string
+     */
+    public function atRuleName();
+
+    /**
+     * @return string
+     */
+
