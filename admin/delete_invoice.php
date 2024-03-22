8e366473204628d05092a3a101d327e0dc4b7951
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:09 +0300

    improve

--- admin/delete_invoice.php
+++ admin/delete_invoice.php
@@ -1,47 +1,46 @@
 <?php
-if(isset($_GET['id']))
-{
+// Check if id is set in the URL
+if (isset($_GET['id'])) {
     include 'database_connection.php';
 
-    
-$id=$_GET['id'];//order id   pkey ---tbl_order
-//tbl_order_item   column for dwnload
-//order_item_quantity
-//item_name
-//$query=mysqli_query("$connn","DELETE FROM tbl_order WHERE order_id=$id")
-$query5 = mysqli_query($conn,"SELECT * FROM tbl_order_item WHERE order_id='$id'");
-$rowquery5 = mysqli_num_rows($query5);
+    // Sanitize the input to prevent SQL injection
+    $id = $_GET['id'];
+    $id = mysqli_real_escape_string($conn, $id);
 
-$query = "SELECT item_name,order_item_quantity FROM tbl_order_item WHERE order_id=$id";
+    // Get the order items for the given order id
+    $query5 = mysqli_query($conn, "SELECT * FROM tbl_order_item WHERE order_id='$id'");
+    $rowquery5 = mysqli_num_rows($query5);
 
+    // Prepare the query to fetch the order items
+    $query = "SELECT item_name, order_item_quantity FROM tbl_order_item WHERE order_id = ?";
+    $stmt = $conn->prepare($query);
 
-for ($x = 1; $x <= $rowquery5 ; $x++)
-{
+    if ($stmt) {
+        // Bind the parameter
+        $stmt->bind_param("i", $id);
 
-if ($stmt = $conn->prepare($query)) {
-    $stmt->execute();
-    
-    $stmt->bind_result($item_name, $order_item_quantity);
-    while ($stmt->fetch()) {
-        //printf("%s, %s\n", $item_name, $order_item_quantity);
-        $name=$item_name;
-        $qty=$order_item_quantity;
-        
-       
-    }
-    echo $name;
-    echo $qty;
-    $query8=mysqli_query($conn,"UPDATE product SET p_quantity=(p_quantity+$qty) WHERE p_name='$name'");
-    $query6 = mysqli_query($conn,"DELETE FROM tbl_order_item WHERE order_id='$id' AND item_name='$name'");
-    $query7 = mysqli_query($conn,"DELETE FROM tbl_order WHERE order_id='$id'");
-    $stmt->close();
+        // Execute the query
+        $stmt->execute();
 
-}
-}
+        // Bind the result variables
+        $stmt->bind_result($item_name, $order_item_quantity);
 
+        // Loop through the result set
+        while ($stmt->fetch()) {
+            // Concatenate the item names and quantities to be displayed on the next page
+            $names[] = $item_name;
+            $qties[] = $order_item_quantity;
 
+            // Update the product quantity
+            $name = $item_name;
+            $qty = $order_item_quantity;
+            $query8 = mysqli_query($conn, "UPDATE product SET p_quantity = p_quantity + $qty WHERE p_name = '$name'");
+        }
 
-}
-header("Location: admin_invoice.php");
+        // Delete the order items
+        foreach ($names as $name) {
+            $query6 = mysqli_query($conn, "DELETE FROM tbl_order_item WHERE order_id='$id' AND item_name='$name'");
+        }
 
-?>
+        // Delete the order
+        $query7 = mysqli_query($conn, "DELETE FROM
