f7fcc298207eeba7c9213c4c3fabb8d5db4483cb
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:36 +0300

    improve

--- dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/CalcRuleValueList.php
+++ dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Value/CalcRuleValueList.php
@@ -2,13 +2,28 @@
 
 namespace Sabberworm\CSS\Value;
 
-class CalcRuleValueList extends RuleValueList {
-	public function __construct($iLineNo = 0) {
-		parent::__construct(array(), ',', $iLineNo);
-	}
+/**
+ * A list of calc() rule values.
+ */
+class CalcRuleValueList extends RuleValueList
+{
+    /**
+     * Constructs a new CalcRuleValueList instance.
+     *
+     * @param int $iLineNo The line number where this value list was defined.
+     */
+    public function __construct(int $iLineNo = 0) {
+        parent::__construct([], ',', $iLineNo);
+    }
 
-	public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat) {
-		return $oOutputFormat->implode(' ', $this->aComponents);
-	}
-
+    /**
+     * Renders this value list as a string.
+     *
+     * @param \Sabberworm\CSS\OutputFormat $oOutputFormat The output format to use.
+     *
+     * @return string The rendered value list.
+     */
+    public function render(\Sabberworm\CSS\OutputFormat $oOutputFormat): string {
+        return $oOutputFormat->implode(' ', $this->aComponents);
+    }
 }
