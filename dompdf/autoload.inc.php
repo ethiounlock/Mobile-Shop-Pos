d1f912259cdf341c81b1ccbbb806c19f95426d62
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:09 +0300

    improve

--- dompdf/autoload.inc.php
+++ dompdf/autoload.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package dompdf
  * @link    http://dompdf.github.com/
@@ -7,34 +8,31 @@
  * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  */
 
-// HMLT5 Parser
-//FIXME: replace with masterminds HTML5
-//require_once __DIR__ . '/lib/html5lib/Parser.php';
-
-// Sabberworm
-spl_autoload_register(function($class)
+// Sabberworm autoloader
+function sabberwormAutoloader($class)
 {
     if (strpos($class, 'Sabberworm') !== false) {
         $file = str_replace('\\', DIRECTORY_SEPARATOR, $class);
         $file = realpath(__DIR__ . '/lib/php-css-parser/lib/' . (empty($file) ? '' : DIRECTORY_SEPARATOR) . $file . '.php');
+
         if (file_exists($file)) {
             require_once $file;
             return true;
         }
     }
-    return false;
-});
 
-// php-font-lib
+    return false;
+}
+
+spl_autoload_register('sabberwormAutoloader');
+
+// Include php-font-lib
 require_once __DIR__ . '/lib/php-font-lib/src/FontLib/Autoloader.php';
 
-//php-svg-lib
+// Include php-svg-lib
 require_once __DIR__ . '/lib/php-svg-lib/src/autoload.php';
 
-
-/*
- * New PHP 5.3.0 namespaced autoloader
- */
+// Include dompdf's Autoloader
 require_once __DIR__ . '/src/Autoloader.php';
-
 Dompdf\Autoloader::register();
+
