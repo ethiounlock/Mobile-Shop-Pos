a2d9423192ac43aaab89f4522e1de63b790c2372
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:19 +0300

    improve

--- vendor/phenx/php-svg-lib/src/Svg/Tag/Image.php
+++ vendor/phenx/php-svg-lib/src/Svg/Tag/Image.php
@@ -1,22 +1,45 @@
 <?php
-/**
- * @package php-svg-lib
- * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
- */
 
 namespace Svg\Tag;
 
+/**
+ * @package php-svg-lib
+ * @link    http://github.com/PhenX/php-svg-lib
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
+ * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
+ */
+
 class Image extends AbstractTag
 {
+    /**
+     * @var int
+     */
     protected $x = 0;
+
+    /**
+     * @var int
+     */
     protected $y = 0;
+
+    /**
+     * @var int
+     */
     protected $width = 0;
+
+    /**
+     * @var int
+     */
     protected $height = 0;
+
+    /**
+     * @var string|null
+     */
     protected $href = null;
 
-    protected function before($attributes)
+    /**
+     * @param array $attributes
+     */
+    protected function before(array $attributes): void
     {
         parent::before($attributes);
 
@@ -26,37 +49,27 @@ class Image extends AbstractTag
         $this->applyTransform($attributes);
     }
 
-    public function start($attributes)
+    /**
+     * @param array $attributes
+     * @throws \Exception
+     */
+    public function start(array $attributes): void
     {
         $document = $this->document;
-        $height = $this->document->getHeight();
+        $height = $document->getHeight();
         $this->y = $height;
 
-        if (isset($attributes['x'])) {
-            $this->x = $attributes['x'];
-        }
-        if (isset($attributes['y'])) {
-            $this->y = $height - $attributes['y'];
-        }
+        $this->x = isset($attributes['x']) ? (int)$attributes['x'] : 0;
+        $this->y = $height - (isset($attributes['y']) ? (int)$attributes['y'] : 0);
 
-        if (isset($attributes['width'])) {
-            $this->width = $attributes['width'];
-        }
-        if (isset($attributes['height'])) {
-            $this->height = $attributes['height'];
+        $this->width = isset($attributes['width']) ? (int)$attributes['width'] : 0;
+        if ($this->width <= 0) {
+            throw new \Exception('Invalid image width.');
         }
 
-        if (isset($attributes['xlink:href'])) {
-            $this->href = $attributes['xlink:href'];
+        $this->height = isset($attributes['height']) ? (int)$attributes['height'] : 0;
+        if ($this->height <= 0) {
+            throw new \Exception('Invalid image height.');
         }
 
-        $document->getSurface()->transform(1, 0, 0, -1, 0, $height);
-
-        $document->getSurface()->drawImage($this->href, $this->x, $this->y, $this->width, $this->height);
-    }
-
-    protected function after()
-    {
-        $this->document->getSurface()->restore();
-    }
-} 
+        $this->href = isset($attributes
