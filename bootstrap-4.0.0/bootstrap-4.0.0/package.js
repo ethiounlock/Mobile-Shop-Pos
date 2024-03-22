a5b13ac9762db98260ed514d0f506f3264236d0e
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:28 +0300

    improve

--- bootstrap-4.0.0/bootstrap-4.0.0/package.js
+++ bootstrap-4.0.0/bootstrap-4.0.0/package.js
@@ -4,16 +4,32 @@
 
 Package.describe({
   name: 'twbs:bootstrap',  // https://atmospherejs.com/twbs/bootstrap
-  summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
-  version: '4.0.0',
+  summary: 'The most popular front-end framework for developing responsive, mobile-first projects on the web.',
+  version: '4.6.0',
   git: 'https://github.com/twbs/bootstrap.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
+
+  // Only add jQuery as a dependency for the client
   api.use('jquery', 'client');
+
+  // Use the less version of Bootstrap instead of css
   api.addFiles([
-    'dist/css/bootstrap.css',
+    'dist/css/bootstrap.less',
     'dist/js/bootstrap.js'
   ], 'client');
+
+  // Add an optional package dependency for server-side usage
+  api.optionalAssets({
+    server: [
+      'dist/js/bootstrap.js'
+    ]
+  });
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+  api.use('twbs:bootstrap');
 });
