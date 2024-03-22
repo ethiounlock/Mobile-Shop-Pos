9eca7021399ad5d2d4f325af91435232bee278a4
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:21 +0300

    improve

--- vendor/phenx/php-font-lib/src/FontLib/TrueType/Header.php
+++ vendor/phenx/php-font-lib/src/FontLib/TrueType/Header.php
@@ -1,31 +1,47 @@
 <?php
-/**
- * @package php-font-lib
- * @link    https://github.com/PhenX/php-font-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
 
 namespace FontLib\TrueType;
+
+use FontLib\Header;
 
 /**
  * TrueType font file header.
  *
  * @package php-font-lib
  */
-class Header extends \FontLib\Header {
-  protected $def = array(
-    "format"        => self::uint32,
-    "numTables"     => self::uint16,
-    "searchRange"   => self::uint16,
-    "entrySelector" => self::uint16,
-    "rangeShift"    => self::uint16,
-  );
+class Header extends Header
+{
+    protected array $def = [
+        "format"        => self::UINT32,
+        "numTables"     => self::UINT16,
+        "searchRange"   => self::UINT16,
+        "entrySelector" => self::UINT16,
+        "rangeShift"    => self::UINT16,
+    ];
 
-  public function parse() {
-    parent::parse();
+    public const FORMAT_STRING_CONVERSION = [
+        0 => 'None',
+        1 => 'TrueType',
+        2 => 'TrueType Collection (CFF)',
+    ];
 
-    $format                   = $this->data["format"];
-    $this->data["formatText"] = $this->convertUInt32ToStr($format);
-  }
+    public function parse(): void
+    {
+        parent::parse();
+
+        $format = $this->data['format'];
+        $this->data['formatText'] = self::convertUInt32ToStr($format);
+    }
+
+    /**
+     * Convert a uint32 value to its string representation.
+     *
+     * @param int $uint32 The uint32 value to convert.
+     *
+     * @return string The string representation of the uint32 value.
+     */
+    protected static function convertUInt32ToStr(int $uint32): string
+    {
+        return self::FORMAT_STRING_CONVERSION[$uint32] ?? 'Unknown';
+    }
 }
