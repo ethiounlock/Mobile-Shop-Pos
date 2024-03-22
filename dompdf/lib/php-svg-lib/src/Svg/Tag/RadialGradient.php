24d115e38d5647bcd0befba5849ec386d1868846
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:37 +0300

    improve

--- dompdf/lib/php-svg-lib/src/Svg/Tag/RadialGradient.php
+++ dompdf/lib/php-svg-lib/src/Svg/Tag/RadialGradient.php
@@ -2,7 +2,7 @@
 /**
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
  * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
  */
 
@@ -12,6 +12,19 @@ class RadialGradient extends AbstractTag
 {
     public function start($attributes)
     {
+        // Initialize radial gradient properties
+        $this->setAttribute('gradientUnits', 'userSpaceOnUse');
+        $this->setAttribute('spreadMethod', 'pad');
 
+        // Parse and set the cx, cy, and r attributes
+        if (isset($attributes['cx'])) {
+            $this->setAttribute('cx', floatval($attributes['cx']));
+        }
+        if (isset($attributes['cy'])) {
+            $this->setAttribute('cy', floatval($attributes['cy']));
+        }
+        if (isset($attributes['r'])) {
+            $this->setAttribute('r', floatval($attributes['r']));
+        }
     }
-} 
+}
