9d946a8fa4ddccc69ff2779ae651c4f85227a154
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:02 +0300

    improve

--- vendor/phenx/php-font-lib/src/FontLib/Table/Type/os2.php
+++ vendor/phenx/php-font-lib/src/FontLib/Table/Type/os2.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package php-font-lib
  * @link    https://github.com/PhenX/php-font-lib
@@ -10,38 +11,39 @@ namespace FontLib\Table\Type;
 use FontLib\Table\Table;
 
 /**
- * `OS/2` font table.
+ * OS/2 font table class.
  *
  * @package php-font-lib
  */
-class os2 extends Table {
-  protected $def = array(
-    "version"             => self::uint16,
-    "xAvgCharWidth"       => self::int16,
-    "usWeightClass"       => self::uint16,
-    "usWidthClass"        => self::uint16,
-    "fsType"              => self::int16,
-    "ySubscriptXSize"     => self::int16,
-    "ySubscriptYSize"     => self::int16,
-    "ySubscriptXOffset"   => self::int16,
-    "ySubscriptYOffset"   => self::int16,
-    "ySuperscriptXSize"   => self::int16,
-    "ySuperscriptYSize"   => self::int16,
-    "ySuperscriptXOffset" => self::int16,
-    "ySuperscriptYOffset" => self::int16,
-    "yStrikeoutSize"      => self::int16,
-    "yStrikeoutPosition"  => self::int16,
-    "sFamilyClass"        => self::int16,
-    "panose"              => array(self::uint8, 10),
-    "ulCharRange"         => array(self::uint32, 4),
-    "achVendID"           => array(self::char, 4),
-    "fsSelection"         => self::uint16,
-    "fsFirstCharIndex"    => self::uint16,
-    "fsLastCharIndex"     => self::uint16,
-    "typoAscender"        => self::int16,
-    "typoDescender"       => self::int16,
-    "typoLineGap"         => self::int16,
-    "winAscent"           => self::int16,
-    "winDescent"          => self::int16,
-  );
+class Os2 extends Table
+{
+    protected $def = [
+        'version' => self::UINT16,
+        'xAvgCharWidth' => self::INT16,
+        'usWeightClass' => self::UINT16,
+        'usWidthClass' => self::UINT16,
+        'fsType' => self::INT16,
+        'ySubscriptXSize' => self::INT16,
+        'ySubscriptYSize' => self::INT16,
+        'ySubscriptXOffset' => self::INT16,
+        'ySubscriptYOffset' => self::INT16,
+        'ySuperscriptXSize' => self::INT16,
+        'ySuperscriptYSize' => self::INT16,
+        'ySuperscriptXOffset' => self::INT16,
+        'ySuperscriptYOffset' => self::INT16,
+        'yStrikeoutSize' => self::INT16,
+        'yStrikeoutPosition' => self::INT16,
+        'sFamilyClass' => self::INT16,
+        'panose' => [self::UINT8, 10],
+        'ulCharRange' => [self::UINT32, 4],
+        'achVendID' => [self::CHAR, 4],
+        'fsSelection' => self::UINT16,
+        'fsFirstCharIndex' => self::UINT16,
+        'fsLastCharIndex' => self::UINT16,
+        'typoAscender' => self::INT16,
+        'typoDescender' => self::INT16,
+        'typoLineGap' => self::INT16,
+        'winAscent' => self::INT16,
+        'winDescent' => self::INT16,
+    ];
 }
