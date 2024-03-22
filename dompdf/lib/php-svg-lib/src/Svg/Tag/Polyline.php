1db9460a6c48215581f6fd71b0284170b4d7b0c3
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:38 +0300

    improve

--- dompdf/lib/php-svg-lib/src/Svg/Tag/Polyline.php
+++ dompdf/lib/php-svg-lib/src/Svg/Tag/Polyline.php
@@ -2,7 +2,7 @@
 /**
  * @package php-svg-lib
  * @link    http://github.com/PhenX/php-svg-lib
- * @author  Fabien Ménager <fabien.menager@gmail.com>
+ * @author  Fabien MÃ©nager <fabien.menager@gmail.com>
  * @license GNU LGPLv3+ http://www.gnu.org/copyleft/lesser.html
  */
 
@@ -10,22 +10,41 @@ namespace Svg\Tag;
 
 class Polyline extends Shape
 {
-    public function start($attributes)
+    /**
+     * @param string $attributes
+     * @return void
+     */
+    public function start(string $attributes): void
     {
-        $tmp = array();
-        preg_match_all('/([\-]*[0-9\.]+)/', $attributes['points'], $tmp);
+        $tmp = [];
+        $result = preg_match_all('/([\-]*[0-9\.]+)/', $attributes['points'] ?? '', $tmp);
 
-        $points = $tmp[0];
+        if ($result === false) {
+            // Handle error
+            throw new \RuntimeException('preg_match_all failed');
+        }
+
+        $points = $tmp[0] ?? [];
         $count = count($points);
+
+        if ($count < 2) {
+            // Handle error
+            throw new \InvalidArgumentException('Not enough points provided');
+        }
 
         $surface = $this->document->getSurface();
         list($x, $y) = $points;
         $surface->moveTo($x, $y);
 
         for ($i = 2; $i < $count; $i += 2) {
+            if (!isset($points[$i], $points[$i + 1])) {
+                // Handle error
+                throw new \InvalidArgumentException('Invalid points provided');
+            }
+
             $x = $points[$i];
             $y = $points[$i + 1];
             $surface->lineTo($x, $y);
         }
     }
-} 
+}
