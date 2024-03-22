97372d8595b4fd85fee209ee5532c0b3a480d721
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:01:36 +0300

    improve

--- vendor/dompdf/dompdf/src/Positioner/Absolute.php
+++ vendor/dompdf/dompdf/src/Positioner/Absolute.php
@@ -1,10 +1,6 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
+declare(strict_types=1);
 
 namespace Dompdf\Positioner;
 
@@ -15,17 +11,16 @@ use Dompdf\FrameDecorator\AbstractFrameDecorator;
  */
 class Absolute extends AbstractPositioner
 {
-
     /**
      * @param AbstractFrameDecorator $frame
+     * @return void
      */
-    function position(AbstractFrameDecorator $frame)
+    public function position(AbstractFrameDecorator $frame): void
     {
         $style = $frame->get_style();
-
         $p = $frame->find_positionned_parent();
 
-        list($x, $y, $w, $h) = $frame->get_containing_block();
+        [$x, $y, $w, $h] = $frame->get_containing_block();
 
         $top = $style->length_in_pt($style->top, $h);
         $right = $style->length_in_pt($style->right, $w);
@@ -34,32 +29,20 @@ class Absolute extends AbstractPositioner
 
         if ($p && !($left === "auto" && $right === "auto")) {
             // Get the parent's padding box (see http://www.w3.org/TR/CSS21/visuren.html#propdef-top)
-            list($x, $y, $w, $h) = $p->get_padding_box();
+            [$x, $y, $w, $h] = $p->get_padding_box();
         }
 
-        list($width, $height) = [$frame->get_margin_width(), $frame->get_margin_height()];
+        [$width, $height] = [$frame->get_margin_width(), $frame->get_margin_height()];
 
         $orig_style = $frame->get_original_style();
         $orig_width = $orig_style->width;
         $orig_height = $orig_style->height;
 
-        /****************************
-         *
-         * Width auto:
-         * ____________| left=auto | left=fixed |
-         * right=auto  |     A     |     B      |
-         * right=fixed |     C     |     D      |
-         *
-         * Width fixed:
-         * ____________| left=auto | left=fixed |
-         * right=auto  |     E     |     F      |
-         * right=fixed |     G     |     H      |
-         *****************************/
-
+        // Width auto
         if ($left === "auto") {
             if ($right === "auto") {
                 // A or E - Keep the frame at the same position
-                $x = $x + $frame->find_block_parent()->get_current_line_box()->w;
+                $x += $frame->find_block_parent()->get_current_line_box()->w;
             } else {
                 if ($orig_width === "auto") {
                     // C
