4f6e3be5a870d598cb40e51a6a6cfeea8beb8860
ethiounlock <admin@ethiounlock.com>
2024-03-22 19:00:45 +0300

    improve

--- dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Parsing/OutputException.php
+++ dompdf/lib/php-css-parser/lib/Sabberworm/CSS/Parsing/OutputException.php
@@ -3,10 +3,13 @@
 namespace Sabberworm\CSS\Parsing;
 
 /**
-* Thrown if the CSS parsers attempts to print something invalid
-*/
-class OutputException extends SourceException {
-	public function __construct($sMessage, $iLineNo = 0) {
-		parent::__construct($sMessage, $iLineNo);
-	}
+ * Thrown if the CSS parsers attempts to print something invalid
+ */
+class OutputException extends SourceException
+{
+    const INVALID_OUTPUT_MESSAGE = 'Attempted to output invalid value';
+
+    public function __construct($iLineNo = 0) {
+        parent::__construct(self::INVALID_OUTPUT_MESSAGE, $iLineNo);
+    }
 }
